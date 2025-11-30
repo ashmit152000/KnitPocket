59ee0383165ee0aac6c7492957e958d54ef858b7
Ashmit Pathak <39757778+ashmit152000@users.noreply.github.com>
2025-11-30 17:31:12 +0530

    Added notification related changes (#9)
    Co-authored-by: Ashmit Kashyap <ashmitkashyap@192.168.1.16>

--- app/(unauth)/layout.tsx
+++ app/(unauth)/layout.tsx
@@ -13,20 +13,26 @@ export default async function UnauthLayout({
   children: React.ReactNode;
 }) {
   const cookieStore = await cookies();
-    const loginStatus = cookieStore.get("notification_status");
-    const [status, message] = loginStatus?.value?.split(";") || [];
-    console.log(status, message);
+  const loginStatus = cookieStore.get("notification_status");
+
+  const [status, message] = loginStatus?.value?.split(";") || [];
+  console.log(status, message);
   return (
     <>
-       <div className="min-h-screen bg-darker">
-      <UnauthNavBar />
+      <div className="min-h-screen bg-darker">
+        <UnauthNavBar />
 
-      {/* Push content below the navbar */}
-      <main>
-        {loginStatus && <Notification type={status as "success" | "error"} message={message} />}
-        {children}
-      </main>
-    </div>
+        {/* Push content below the navbar */}
+        <main>
+          {loginStatus && (
+            <Notification
+              type={status as "success" | "error"}
+              message={message}
+            />
+          )}
+          {children}
+        </main>
+      </div>
     </>
   );
 }
